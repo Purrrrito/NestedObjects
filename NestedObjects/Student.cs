58a85a22f28a65a4c2089aa9d990d91479568d02
Wyatt Smith <wyattjorgen@gmail.com>
2024-07-11 21:45:34 -0700

    Created student and advisor classes

---
+++ NestedObjects/Student.cs
@@ -0,0 +1,28 @@
+﻿namespace NestedObjects
+{
+	/// <summary>
+	/// Represents an individual college student
+	/// </summary>
+	public class Student
+	{
+		/// <summary>
+		/// The legal first name
+		/// </summary>
+        public string FirstName { get; set; }
+
+		/// <summary>
+		/// The legal last name
+		/// </summary>
+        public string LastName { get; set; }
+
+		/// <summary>
+		/// The assigned school email address
+		/// </summary>
+        public string SchoolEmail { get; set; }
+
+		/// <summary>
+		/// Student's phone number
+		/// </summary>
+        public string PhoneNumber { get; set; }
+    }
+}
